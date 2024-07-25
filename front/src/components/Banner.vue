8635ddd394b5d26c94907faa6fa69ac04a9f6d34
gujialong <gu_jia_long@163.com>
2024-07-25 23:58:45 +0800

    feat：首页大概样式

---
+++ front/src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full h-64 bg-cover bg-center" :style="{ backgroundImage: 'url(/path/to/banner.jpg)' }">
+      <div class="bg-black bg-opacity-50 h-full flex items-center justify-center">
+        <h1 class="text-white text-4xl font-bold">Banner Title</h1>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Banner',
+  };
+  </script>
+  
---
+++ front/src/components/CategoryList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <div v-for="category in categories" :key="category.id" class="relative bg-cover bg-center h-48" :style="{ backgroundImage: `url(${category.image})` }">
+        <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 text-center">
+          {{ category.title }}
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CategoryList',
+    data() {
+      return {
+        categories: [
+          { id: 1, title: 'Category 1', image: '/path/to/category1.jpg' },
+          { id: 2, title: 'Category 2', image: '/path/to/category2.jpg' },
+          { id: 3, title: 'Category 3', image: '/path/to/category3.jpg' },
+        ],
+      };
+    },
+  };
+  </script>
+  
